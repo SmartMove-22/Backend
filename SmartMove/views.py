f0a29f52224773e2b77ee48966042cf6786ea437
Martinho Tavares <martinho.tavares@ua.pt>
2022-10-12 23:11:54 +0100

    Fixed returned response from exercise analysis endpoint

--- SmartMove/views.py
+++ SmartMove/views.py
@@ -14,45 +14,11 @@ from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
 
 from django.core.exceptions import ObjectDoesNotExist
 
-from SmartMove.models import Trainee, Coach
+from SmartMove.models import RealTimeReport, Trainee, Coach
 from SmartMove.serializers import RealTimeReportSerializer, UserSerializer, TraineeSerializer, CoachSerializer
 from .smart_move_analysis.reference_store import LandmarkData
 from .smart_move_analysis.utils import landmark_list_angles
 
-
-@api_view(['POST'])
-def exercise_analysis(request):
-
-    time = int(request.data['time'])
-    repetition_half = int(request.data['repetition_half'])
-    exercise_category = request.data['exercise_category']
-    landmarks_coordinates = [request.data[str(i)] for i in range(33)]
-
-    smartmoveConfig = apps.get_app_config('SmartMoveConfig')
-
-    knn_model = smartmoveConfig.knn_models[(exercise_category, repetition_half == 1)]
-
-    if knn_model:
-        landmark_angles = landmark_list_angles([
-            LandmarkData(visibility=None, **coordinates) for coordinates in landmarks_coordinates
-        ])
-        correctness, most_divergent_angle_value, most_divergent_angle_idx = knn_model.correctness(landmark_angles)
-        progress = knn_model.progress(landmark_angles)
-    else:
-        # TODO: error response
-        pass
-
-    # TODO: repetitions aren't tracked yet (the value of 'repetition')
-    repetition = 0
-    if progress > 0.95:
-        repetition_half = 1 if repetition_half == 2 else 2
-        if repetition_half == 1:
-            repetition = 1
-
-    # Convert to Python data types that can then be easily rendered into JSON (Example)
-    response = RealTimeReportSerializer(correctness=correctness, progress=progress, repetition=repetition, repetition_half=repetition_half)
-
-    return Response(response, status=status.HTTP_200_OK)
 
 all_tokens = {}
 
@@ -317,3 +283,43 @@ def coach_profile(request):
         "Content": CoachSerializer(coach).data,
         "Code": "HTTP_200_OK",
     }, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def exercise_analysis(request, exerciseId):
+
+    time = int(request.data['time'])
+    repetition_half = int(request.data['repetition_half'])
+    exercise_category = request.data['exercise_category']
+    landmarks_coordinates = [request.data[str(i)] for i in range(33)]
+
+    smartmoveConfig = apps.get_app_config('SmartMove')
+
+    if (exercise_category, repetition_half) not in smartmoveConfig.knn_models:
+        # TODO: error response (exercise not present)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    knn_model = smartmoveConfig.knn_models[(exercise_category, repetition_half == 1)]
+
+    if knn_model:
+        landmark_angles = landmark_list_angles([
+            LandmarkData(visibility=None, **coordinates) for coordinates in landmarks_coordinates
+        ])
+        correctness, most_divergent_angle_value, most_divergent_angle_idx = knn_model.correctness(landmark_angles)
+        progress = knn_model.progress(landmark_angles)
+    else:
+        # TODO: error response
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    # TODO: repetitions aren't tracked yet (the value of 'repetition')
+    repetition = 0
+    if progress > 0.95:
+        repetition_half = 1 if repetition_half == 2 else 2
+        if repetition_half == 1:
+            repetition = 1
+
+    # Convert to Python data types that can then be easily rendered into JSON (Example)
+    report = RealTimeReport.objects.create(correctness=correctness, progress=progress, repetition=repetition, repetition_half=repetition_half)
+    response = RealTimeReportSerializer(report)
+
+    return Response(response.data, status=status.HTTP_200_OK)
