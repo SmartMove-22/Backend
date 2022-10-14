a1a3bc32d051c5d18f26c5494a38db14bcb4e00e
Martinho Tavares <martinho.tavares@ua.pt>
2022-10-14 12:10:39 +0100

    Bugfixing RealTimeReport, dynamic exercise loading from MongoDB

--- SmartMove/apps.py
+++ SmartMove/apps.py
@@ -17,10 +17,10 @@ class SmartmoveConfig(AppConfig):
             connection_string=f'mongodb://{environ.get("MONGO_USER", "username")}:{environ.get("MONGO_PASS", "password")}@mongodb'
         )
 
-        self.knn_models = {exercise_half:KNNRegressor.from_exercise_references(self.reference_store.get(*exercise_half))
-            for exercise_half in [
-                ('squat', True), ('squat', False),
-                ('sit-up', True), ('sit-up', False),
-                ('pushup', True), ('pushup', False)
-            ]
-        }
+        self.knn_models = {}
+        for exercise in self.reference_store.exercises():
+            self.knn_models.extend({exercise_half:KNNRegressor.from_exercise_references(self.reference_store.get(*exercise_half))
+                for exercise_half in [
+                    (exercise, True), (exercise, False)
+                ]
+            })
